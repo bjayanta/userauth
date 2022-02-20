992621f16d32cb5d3de3b07cb6e1f24e353dfe6e
Jayanta <bjayanta.me@gmail.com>
2022-02-20 19:11:27 +0600

    add validation errors in template.

--- users/views.py
+++ users/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, HttpResponseRedirect
 from .forms import SignUpForm, UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
 # Create your views here.
 def registration(request):
-    form = SignUpForm(request.POST)
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
 
-    if form.is_valid:
-        pass
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            form.save()
+
+            newUser = authenticate(username=username, password=password)
+            if newUser is not None:
+                login(request, newUser)
+                HttpResponseRedirect('website.index')
     else:
         form = SignUpForm()
 
