fdcc7670c6153dab1a44ed63bbe2d485d2130b81
Jayanta <bjayanta.me@gmail.com>
2022-02-24 22:00:53 +0600

    add login required decorator

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
 from multiprocessing import context
+import django
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     context = {}
     return render(request, 'core/index.html', context)
 
+@login_required(login_url='users.signin')
 def dashboard(request):
     context = {}
     return render(request, 'core/dashboard.html', context)
